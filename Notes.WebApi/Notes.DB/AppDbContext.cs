c7359e4de562e06a8e44aacbe36bc14365df9ecd
Andrew Stanton (Beyondsoft Consulting <v-astanton@microsoft.com>
2021-06-28 14:43:06 -0700

    api commit

---
+++ Notes.WebApi/Notes.DB/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Notes.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        // The following configures EF to create a Sqlite database file as `C:\blogging.db`.
+        // For Mac or Linux, change this to `/tmp/blogging.db` or any other absolute path.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Notes;Integrated Security=True");
+        }
+
+    }
+}
